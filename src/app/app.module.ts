dfb705a6b2bdc0cde895430378c35642b1872cbe
omarVodiaK <omar.boudfoust@gmail.com>
2020-10-12 01:15:51 +0800

    update dependency to use published package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RawModule } from 'angular-components';
+import { RawModule } from 'raw-angular-components/dist';
 
 @NgModule({
   declarations: [AppComponent],
